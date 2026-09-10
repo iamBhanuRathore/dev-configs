c102a73bee6ffc8facd35b0c3d1b9be287bee6d1
Bhanu Rathore <rathoreb853@gmail.com>
2026-09-11 02:22:14 +0530

    feat: move neo-tree sidebar to the right

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,9 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      window = {
+        position = "right", -- sidebar on the right (like Antigravity/VS Code)
+      },
       filesystem = {
         filtered_items = {
           always_show = {
